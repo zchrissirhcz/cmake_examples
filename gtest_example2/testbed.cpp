534b67074d9c14006049b3365557a4bd4f0ca749
Zhuo Zhang <imzhuo@foxmail.com>
2021-09-03 21:05:30 +0800

    fix: minor fix on gtest example2

--- gtest_example2/testbed.cpp
+++ gtest_example2/testbed.cpp
@@ -13,9 +13,28 @@ TEST(OpenCV, read_image)
     EXPECT_TRUE(image.empty()==false);
 }
 
+TEST(OpenCV, cvtColor)
+{
+    std::string image_path = "hello.jpg";
+    cv::Mat bgr = cv::imread(image_path);
+	ASSERT_TRUE(!bgr.empty());
+
+	cv::Mat gray;
+	cv::cvtColor(bgr, gray, cv::COLOR_BGR2GRAY);
+	EXPECT_TRUE(gray.size()==bgr.size());
+}
+
 
 int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);
+
+    // Run a specific test only
+    //testing::GTEST_FLAG(filter) = "MyLibrary.TestReading"; // I'm testing a new feature, run something quickly
+    testing::GTEST_FLAG(filter) = "OpenCV.read_image";
+
+    // Exclude a specific test
+    //testing::GTEST_FLAG(filter) = "-cvtColorTwoPlane.yuv420sp_to_rgb:-cvtColorTwoPlane.rgb_to_yuv420sp"; // The writing test is broken, so skip it
+
     return RUN_ALL_TESTS();
 }
