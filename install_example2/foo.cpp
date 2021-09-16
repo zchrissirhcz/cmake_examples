335616b0bcfb6ab54c36467d3eaf7921fad6de3f
Zhuo Zhang <imzhuo@foxmail.com>
2021-09-17 00:01:09 +0800

    feat: add install_example2, demonstrate PUBLIC_HEADER usage

---
+++ install_example2/foo.cpp
@@ -0,0 +1,14 @@
+#include "foo.h"
+#include <stdio.h>
+
+void hello(const char* name)
+{
+    if (name)
+    {
+        printf("hello, %s\n", name);
+    }
+    else
+    {
+        printf("hello, world!\n");
+    }
+}
