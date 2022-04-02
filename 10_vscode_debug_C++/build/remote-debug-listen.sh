5f64f103cd0f34652a0e47353d7d0927707b8f69
Zhuo Zhang <imzhuo@foxmail.com>
2022-04-02 21:41:25 +0800

    [feat] Add VSCode remote debug Android executable configs

---
+++ 10_vscode_debug_C++/build/remote-debug-listen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# simple. sometimes not working
+#adb shell "cd /data/local/tmp; ./lldb-server platform --listen \*:10086 --server"
+
+
+
+# useless, too long
+# # 2. forward
+# adb forward tcp:10088 tcp:10086
+
+# # 1. listen
+adb shell
+cd /data/local/tmp
+./lldb-server platform --listen *:10086 --server
