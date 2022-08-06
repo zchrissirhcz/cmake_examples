cae38bcb809e13a4b078960522f44924fda9fa27
Zhuo Zhang <imzhuo@foxmail.com>
2022-08-06 18:53:59 +0800

    feat: add tools/msvc_dynamic_crt.cmake

---
+++ tools/msvc_dynamic_crt.cmake
@@ -0,0 +1,12 @@
+# Enables use of dynamically linked CRT
+# i.e. use MD / MDd
+
+# 设置策略CMP0091为NEW，新策略
+if (POLICY CMP0091)
+  cmake_policy(SET CMP0091 NEW)
+endif (POLICY CMP0091)
+
+if(MSVC AND NOT CMAKE_VERSION VERSION_LESS "3.15")
+  # cmake before version 3.15 not work
+  set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreadedDLL$<$<CONFIG:Debug>:Debug>")
+endif()
