7a89975c3220dd3b781b74a0031a012db9dc9b2c
Zhuo Zhang <imzhuo@foxmail.com>
2023-04-07 10:08:30 +0800

    specify output name

---
+++ 2_global_and_target_settings/12_specify_output_name/hello.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+#include <stdio.h>
+
+void hello(const char* name)
+{
+    printf("hello, %s\n", name);
+}
---
+++ 2_global_and_target_settings/12_specify_output_name/testbench.cpp
@@ -0,0 +1,10 @@
+#include "hello.h"
+#include "world.h"
+
+int main()
+{
+    hello("C++");
+    world("CMake");
+
+    return 0;
+}
