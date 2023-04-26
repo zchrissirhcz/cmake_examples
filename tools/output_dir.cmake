0f22f841f996223b584b897f71ae66a795a00065
Zhuo Zhang <imzhuo@foxmail.com>
2023-04-26 16:12:12 +0800

    tools/output_dir.cmake: Fix output directory for Visual Studio

--- tools/output_dir.cmake
+++ tools/output_dir.cmake
@@ -1,5 +1,12 @@
+#======================================================================
+# Author:   Zhuo Zhang <imzhuo@foxmail.com>
+# Created:  2023-04-26 15:50:01
+# Modified: 2023-04-26 15:50:07
+#======================================================================
+
 # Save libs and executables in the same directory
-if(NOT EXECUTABLE_OUTPUT_PATH)
+# MSVC will have value for EXECUTABLE_OUTPUT_PATH on default, let's replace it
+if((NOT EXECUTABLE_OUTPUT_PATH) OR (CMAKE_C_COMPILER_ID STREQUAL "MSVC"))
   set(EXECUTABLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}" CACHE PATH "Output directory for applications")
   set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH} CACHE INTERNAL "") # static
   set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH} CACHE INTERNAL "") # shared
