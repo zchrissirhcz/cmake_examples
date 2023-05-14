2a7875aacbf175ff79cd0437997de2a3f474af92
Zhuo Zhang <imzhuo@foxmail.com>
2023-05-14 13:29:41 +0800

    update fmtlib example usage

--- 1_package_management/use_installed_packages/use_fmt/main.cpp
+++ 1_package_management/use_installed_packages/use_fmt/main.cpp
@@ -1,10 +1,11 @@
 #include <fmt/core.h>
-
+#include <iostream>
 
 int main()
 {
     fmt::print("Hello, world!\n");
-    fmt::format("hello, {:s}", "world");
+    std::cout << fmt::format("hello, {:s}\n", "world");
+    std::cout << fmt::format("PI is {:6.2f}\n", 3.1415926);
 
     return 0;
 }
