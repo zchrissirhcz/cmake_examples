dcb192ffdf9c35cbcd1b64f79c665fdbcfbdd605
Zhuo Zhang <imzhuo@foxmail.com>
2023-05-15 22:06:18 +0800

    feat(cvpkg.cmake): detect and copy remaining {.dll,.so.,.dylib} files

--- tools/cvpkg.cmake
+++ tools/cvpkg.cmake
@@ -11,7 +11,7 @@ set(CVPKG_INCLUDE_GUARD 1)
 #======================================================================
 set(CVPKG_AUTHOR "Zhuo Zhang <imzhuo@foxmail.com>")
 set(CVPKG_CREATE_TIME "2023.04.23 13:00:00")
-set(CVPKG_VERSION "2023-04-26 19:26:43")
+set(CVPKG_VERSION "2023-05-15 15:57:27")
 set(CVPKG_VERBOSE 1)
 
 #======================================================================
@@ -147,31 +147,106 @@ function(cvpkg_copy_imported_lib targetName dstDir)
     return()
   endif()
 
+  if(CMAKE_SYSTEM_NAME MATCHES "Windows")
+    set(shared_library_filename_ext ".dll")
+  elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
+    set(shared_library_filename_ext ".so")
+  elseif(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+    set(shared_library_filename_ext ".dylib")
+  endif()
+
   get_target_property(pkg_type ${targetName} TYPE)
   if(NOT (${pkg_type} STREQUAL "SHARED_LIBRARY"))
+    if(${pkg_type} STREQUAL "STATIC_LIBRARY")
+
+      if(CMAKE_SYSTEM_NAME MATCHES "Windows")
+        set(static_library_filename_ext ".lib")
+      elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
+        set(static_library_filename_ext ".a")
+      elseif(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+        set(static_library_filename_ext ".a")
+      endif()
+
+      ### for static library targets, there might be `bin` directory, parallel to `lib` directory.
+      # 先获取静态库文件路径
+      foreach(prop ${prop_lst})
+        get_target_property(static_library_path ${pkg} ${prop})
+        if(static_library_path)
+          # 获取静态库所在目录
+          get_filename_component(static_library_live_directory ${static_library_path} DIRECTORY)
+          # 获取静态库目录的上层目录
+          get_filename_component(static_library_parent_directory ${static_library_live_directory} DIRECTORY)
+          set(candidate_bin_dir "${static_library_parent_directory}/bin")
+          # 判断上层目录是否存在 bin 目录, 如果存在 bin 目录， 执行扫描和拷贝
+          if(EXISTS "${candidate_bin_dir}")
+            set(glob_pattern "${candidate_bin_dir}/*${shared_library_filename_ext}")
+            file(GLOB shared_library_path_lst "${glob_pattern}")
+            foreach(shared_library_path ${shared_library_path_lst})
+              list(APPEND copied_shared_library_path_lst "${shared_library_path}")
+              cvpkg_info("Copy ${shared_library_filename_ext} file (for static library, we detect and copy them!)")
+              cvpkg_info("  - shared library file: ${prop}=${static_library_path}")
+              cvpkg_info("  - dstDir: ${dstDir}")
+              execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${shared_library_path} ${dstDir})
+            endforeach()
+          endif()
+        endif()
+      endforeach()
+    endif()
+
     return()
   endif()
 
-  if(CMAKE_SYSTEM_NAME MATCHES "Windows")
-    set(shared_library_name_type ".dll")
-  elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
-    set(shared_library_name_type ".so")
-  elseif(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-    set(shared_library_name_type ".dylib")
-  endif()
 
+  ### copy as the package description file (xxx-config.cmake or xxx.cmake) decribed
   set(pkg ${targetName})
+  set(copied_shared_library_path_lst "")
   foreach(prop ${prop_lst})
     cvpkg_debug("!! prop: ${prop}")
     get_target_property(shared_library_path ${pkg} ${prop})
     if(shared_library_path)
-      cvpkg_info("Copy ${shared_library_name_type} file")
+      list(APPEND copied_shared_library_path_lst "${shared_library_path}")
+      cvpkg_info("Copy ${shared_library_filename_ext} file")
       cvpkg_info("  - package(target): ${pkg}")
       cvpkg_info("  - prop: ${prop}=${shared_library_path}")
       cvpkg_info("  - dstDir: ${dstDir}")
       execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${shared_library_path} ${dstDir})
     endif()
   endforeach()
+
+  ### copy un-tracked shared library files that under same directory of each tracked shared library files
+  cvpkg_is_list_empty(copied_shared_library_path_lst copied_shared_library_path_lst_empty)
+  if(${copied_shared_library_path_lst_empty})
+    return()
+  endif()
+
+  # get directories of each copied shared library files
+  set(shared_library_live_directory_lst "")
+  foreach(copied_shared_library_path ${copied_shared_library_path_lst})
+    get_filename_component(shared_library_live_directory ${copied_shared_library_path} DIRECTORY)
+    list(APPEND shared_library_live_directory_lst "${shared_library_live_directory}")
+  endforeach()
+
+  # remove duplicated directories
+  list(REMOVE_DUPLICATES "${shared_library_live_directory_lst}")
+
+  # for each candidate directory, scan shared library files
+  foreach(shared_library_live_directory ${shared_library_live_directory_lst})
+    set(glob_pattern "${shared_library_live_directory}/*${shared_library_filename_ext}")
+    file(GLOB shared_library_path_lst "${glob_pattern}")
+    foreach(shared_library_path ${shared_library_path_lst})
+      # if the scanned shared library file is not copied, do a copy
+      cvpkg_is_item_in_list(copied_shared_library_path_lst "${shared_library_path}" shared_library_already_copied)
+      if(NOT shared_library_already_copied)
+        list(APPEND copied_shared_library_path_lst "${shared_library_path}")
+        cvpkg_info("Copy ${shared_library_filename_ext} file (xxx-config.cmake forget this file, but we copy them!)")
+        cvpkg_info("  - package(target): ${pkg}")
+        cvpkg_info("  - prop: ${prop}=${shared_library_path}")
+        cvpkg_info("  - dstDir: ${dstDir}")
+        execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${shared_library_path} ${dstDir})
+      endif()
+    endforeach()
+  endforeach()
+
 endfunction()
 
 #======================================================================
@@ -183,7 +258,8 @@ endfunction()
 #----------------------------------------------------------------------
 function(cvpkg_copy_required_dlls targetName dstDir)
   cvpkg_get_flatten_requires(testbed flatten_pkgs)
-  cvpkg_debug("flatten_pkgs: ${flatten_pkgs}")
+  #cvpkg_debug("flatten_pkgs: ${flatten_pkgs}")
+  message(STATUS "flatten_pkgs: ${flatten_pkgs}")
   foreach(pkg ${flatten_pkgs})
    cvpkg_copy_imported_lib(${pkg} ${dstDir})
   endforeach()
