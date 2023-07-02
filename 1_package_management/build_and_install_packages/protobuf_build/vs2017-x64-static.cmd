447c5c526459960a45362207649d9861c09fe2d1
Zhuo Zhang <imzhuo@foxmail.com>
2023-07-02 22:55:45 +0800

    add protobuf build scripts with VS2017

---
+++ 1_package_management/build_and_install_packages/protobuf_build/vs2017-x64-static.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+set BUILD_DIR=vs2017-x64-static
+cmake ^
+    -S .. ^
+    -B %BUILD_DIR% ^
+    -G "Visual Studio 15 2017 Win64" ^
+    -Dprotobuf_BUILD_TESTS=OFF ^
+    -DCMAKE_INSTALL_PREFIX=D:/artifacts/protobuf/v3.21.9/vs2017-x64 ^
+    -Dprotobuf_MSVC_STATIC_RUNTIME=OFF ^
+    -DBUILD_SHARED_LIBS=OFF
+
+cmake --build %BUILD_DIR% --config Debug && cmake --build %BUILD_DIR% --config Debug --target INSTALL
+
+cmake --build %BUILD_DIR% --config Release && cmake --build %BUILD_DIR% --config Release --target INSTALL
+
+
+pause
