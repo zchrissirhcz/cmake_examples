2ab8d84b0467880529a46ba7e76d6f556eb04589
Zhuo Zhang <imzhuo@foxmail.com>
2023-07-15 23:35:32 +0800

    [docs] Fix typos in msvc_exception_fh3.cmake

--- tools/msvc_exception_fh3.cmake
+++ tools/msvc_exception_fh3.cmake
@@ -1,15 +1,15 @@
 # Author: Zhuo Zhang <imzhuo@foxmail.com>
 # Homepage: https://github.com/zchrissirhcz
-# Last update: 2023-06-26 11:52:17
+# Last update: 2023-07-15 23:34:00
 
 # Scenario:
 # - build an library `hello` with VS2019 >= 16.3 (or VS2022), which uses Frame Handler 4 on default
-# - link this librayy `hello` in VS2017/VS2015, or VS2019 < 16.3, which uses Frame Handler 4 on default
+# - link this library `hello` in VS2017/VS2015, or VS2019 < 16.3, which uses Frame Handler 4 on default
 # - then comes link error: unresolved external symbol __CxxFrameHandler4
 # For a full example, see https://github.com/zchrissirhcz/min-repros/tree/master/test_GS_EHsc_link_error
 #
 
-option(USE_COMPATIBLE_FRAME_HANDLER_FOR_VS "Switch off FH4 feature(then use FH3) for compatibilitie?" ON)
+option(USE_COMPATIBLE_FRAME_HANDLER_FOR_VS "Switch off FH4 feature(then use FH3) for compatibility?" ON)
 
 if(USE_COMPATIBLE_FRAME_HANDLER_FOR_VS)
   if((CMAKE_CXX_COMPILER_ID STREQUAL "MSVC") AND (CMAKE_CXX_COMPILER_VERSION STRGREATER_EQUAL 16.3))
