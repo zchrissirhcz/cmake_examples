a37b2082994d0b9b5f3f1fa49b8fe7d7690b143f
Zhuo Zhang <imzhuo@foxmail.com>
2023-08-27 00:36:35 +0800

    [feat] clang-tidy.cmake now support multiple source files

--- tools/clang-tidy.cmake
+++ tools/clang-tidy.cmake
@@ -1,6 +1,6 @@
 # Author: Zhuo Zhang <imzhuo@foxmail.com>
 # Homepage: https://github.com/zchrissirhcz
-# Last update: 2023-08-21 22:53:00
+# Last update: 2023-08-27 00:37:00
 
 # This cmake plugin let you run clang-tidy on specified target
 # Each time you run "make", it report warnings if potential bug found
@@ -24,13 +24,32 @@ else()
 endif()
 
 function(cvpkg_apply_clang_tidy targetName)
+  # collecting absolute paths for each source file in the given target
   get_target_property(target_sources ${targetName} SOURCES)
   get_target_property(target_source_dir ${targetName} SOURCE_DIR)
-  message(STATUS "target_sources: ${target_sources}")
-  message(STATUS "target_source_dir: ${target_source_dir}")
+  # message(STATUS "target_source_dir: ${target_source_dir}")
+  # message(STATUS "target_sources:")
+  set(src_path_lst "")
+  foreach(target_source ${target_sources})
+    # message(STATUS "   ${target_source}")
+    if(IS_ABSOLUTE ${target_source})
+      set(target_source_absolute_path ${target_source})
+    else()
+      set(target_source_absolute_path ${target_source_dir}/${target_source})
+    endif()
+    list(APPEND src_path_lst ${target_source_absolute_path})
+  endforeach()
+
+  # prepare clang-tidy command with arguments
+  set(clang_tidy_raw_command "${CLANG_TIDY_EXE} -p ${CMAKE_BINARY_DIR} ${src_path_lst}")
+  
+  # cmake only accept semicolon(;) separated list when a command with arguments is given
+  string(REPLACE " " ";" clang_tidy_converted_command "${clang_tidy_raw_command}")
+  # message(STATUS "clang_tidy_raw_command: ${clang_tidy_raw_command}")
+  # message(STATUS "clang_tidy_converted_command: ${clang_tidy_converted_command}")
   add_custom_target(
     clang-tidy
-    COMMAND ${CLANG_TIDY_EXE} -p ${CMAKE_BINARY_DIR} ${target_source_dir}/${target_sources}
+    COMMAND ${clang_tidy_converted_command}
   )
   add_dependencies(${targetName} clang-tidy)
 endfunction()
