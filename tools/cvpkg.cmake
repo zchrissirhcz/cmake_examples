ccba0f2f20d423a688d966e36d71dbb74e3cbdc5
Zhuo Zhang <imzhuo@foxmail.com>
2023-10-07 09:57:53 +0800

    [refactor] Remove output dir settings in cvpkg.cmake

--- tools/cvpkg.cmake
+++ tools/cvpkg.cmake
@@ -35,15 +35,6 @@ endif()
 # postfix for debug type library files
 set(CMAKE_DEBUG_POSTFIX "_d")
 
-# Save libs and executables in the same directory
-# MSVC will have value for EXECUTABLE_OUTPUT_PATH on default. Now we ignore it.
-if(NOT EXECUTABLE_OUTPUT_PATH)
-  set(EXECUTABLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}" CACHE PATH "Output directory for applications")
-  set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH} CACHE INTERNAL "") # static
-  set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH} CACHE INTERNAL "") # shared
-  set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH} CACHE INTERNAL "") # exe
-endif()
-
 # we use c++17
 set(CMAKE_CXX_STANDARD 17)
 
