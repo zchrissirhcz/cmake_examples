7c7185da2d29b266b32b9737e58daef25afa9365
Zhuo Zhang <imzhuo@foxmail.com>
2023-11-24 09:47:32 +0800

    [feat] Add -G "Ninja Multi-Config" example

--- 3_cross_compilation/generator_example/build/vs2022-x64-ninja-multiconfig.cmd
+++ 3_cross_compilation/generator_example/build/vs2022-x64-ninja-multiconfig.cmd
@@ -1,9 +1,8 @@
 @echo off
 
-@REM call enter-vs2022-x64-ninja-env.cmd
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-set BUILD_DIR=vs2022-x64-ninja
+set BUILD_DIR=vs2022-x64-ninja-multiconfig
 
 set CC=cl
 set CXX=cl
@@ -11,9 +10,9 @@ set CXX=cl
 cmake ^
   -S .. ^
   -B %BUILD_DIR% ^
-  -G Ninja ^
-  -DCMAKE_BUILD_TYPE=Debug
+  -G "Ninja Multi-Config"
 
-cmake --build %BUILD_DIR%
+cmake --build %BUILD_DIR% --config Debug
+cmake --build %BUILD_DIR% --config Release
 
 pause
