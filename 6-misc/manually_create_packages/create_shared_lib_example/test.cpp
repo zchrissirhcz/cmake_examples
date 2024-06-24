7333ab00bdee00dbcb85b5fbcd94108ef2d0965d
Zhuo Zhang <imzhuo@foxmail.com>
2024-06-24 23:37:54 +0800

    [clean] Delete and rename old create dll examples

--- 6-misc/manually_create_packages/create_shared_lib_example/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include "get_three.h"
-#include "get_seven.h"
-
-int main() {
-    printf("PublicGetThree returned %d\n", PublicGetThree());
-    printf("PublicGetSeven returned %d\n", PublicGetSeven());
-}
