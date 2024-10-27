4fa361b60c1c54e104daca0aaf2533a7097289d6
Zhuo Zhang <imzhuo@foxmail.com>
2024-10-27 17:08:01 +0800

    Use Catch2 for unittest

---
+++ 4-import-packages/11_use_installed_packages/use_cache2/test.cpp
@@ -0,0 +1,22 @@
+#include <catch2/catch_test_macros.hpp>
+
+unsigned int Factorial(unsigned int number)
+{
+    return number <= 1 ? number : Factorial(number - 1) * number;
+}
+
+TEST_CASE("Factorials are computed, succ", "[factorial]")
+{
+    REQUIRE(Factorial(1) == 1);
+    REQUIRE(Factorial(2) == 2);
+    REQUIRE(Factorial(3) == 6);
+    REQUIRE(Factorial(10) == 3628800);
+}
+
+TEST_CASE("Factorials are computed, fail", "[factorial_failed]")
+{
+    REQUIRE(Factorial(1) == 1);
+    REQUIRE(Factorial(2) == 2);
+    REQUIRE(Factorial(3) == 6);
+    REQUIRE(Factorial(10) == 3628801);
+}
