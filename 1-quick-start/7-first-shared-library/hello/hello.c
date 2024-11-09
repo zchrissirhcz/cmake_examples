caca17d675fa83e3cc36816b752e1d63d1e1fe55
Zhuo Zhang <imzhuo@foxmail.com>
2024-11-09 20:09:19 +0800

    Hide symbols for shared library

--- 1-quick-start/7-first-shared-library/hello/hello.c
+++ 1-quick-start/7-first-shared-library/hello/hello.c
@@ -1,17 +1,27 @@
 #include "hello.h"
 #include <stdio.h>
+#include <stdarg.h>
+
+void print(const char* msg, ...)
+{
+    va_list args;
+    va_start(args, msg);
+    vprintf(msg, args);
+    va_end(args);
+    printf("\n");
+}
 
 void hello_init()
 {
-    printf("hello_init() ok\n");
+    print("hello_init() ok");
 }
 
 void hello_destroy()
 {
-    printf("hello_destroy() ok\n");
+    print("hello_destroy() ok");
 }
 
 void hello_run(const char* name)
 {
-    printf("hello, %s!\n", name);
+    print("hello, %s!", name);
 }
